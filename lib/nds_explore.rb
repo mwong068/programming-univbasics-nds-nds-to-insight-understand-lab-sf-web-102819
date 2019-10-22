180e8c9336c0377b25a417ada90554c6f61e3eb1
Megan Wong <mwong068@gmail.com>
2019-10-22 02:44:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,7 +7,7 @@ def pretty_print_nds(nds)
   pp nds
 end
 
-def print_first_directors_movie_titles
+def print_first_directors_movie_titles(directors_database)
   i = 0 
   while i < directors_database.length do
     j = 0 
